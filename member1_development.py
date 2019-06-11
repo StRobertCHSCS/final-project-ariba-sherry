e8bc8efff0905943b48abed8d77ef95f72a84e7b
sherrycao0 <48096482+sherrycao0@users.noreply.github.com>
2019-06-11 10:35:57 -0400

    Connect4: Wrote the code to check for horizontal, vertical, and one of the diagonal wins

--- member1_development.py
+++ member1_development.py
@@ -1,1 +1,22 @@
 
+# check horizontal spaces
+for y in range(boardHeight):
+    for x in range(boardWidth - 3):
+        if tile != 0 and grid[x][y] == tile and grid[x + 1][y] == tile and grid[x + 2][y] == tile and grid[x + 3][ y] == tile:
+            team(curr)
+            return True
+
+# check vertical spaces
+for x in range(boardWidth):
+    for y in range(boardHeight - 3):
+        if  tile != 0 and grid[x][y] == tile and grid[x][y + 1] == tile and grid[x][y + 2] == tile and grid[x]
+            [y + 3] == tile:
+            team(curr)
+            return True
+
+# check / diagonal spaces
+for x in range(boardWidth - 3):
+    for y in range(3, boardHeight):
+        if tile != 0 and grid[x][y] == tile and grid[x + 1][y - 1] == tile and grid[x + 2][y - 2] == tile and grid[x + 3][y - 3] == tile:
+            team(curr)
+            return True
