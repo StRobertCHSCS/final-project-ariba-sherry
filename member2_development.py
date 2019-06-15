ebfbb59390cee15ea843544649841b5e48773475
sherrycao0 <48096482+sherrycao0@users.noreply.github.com>
2019-06-15 18:50:01 -0400

    Connect4: Using the new array backed grid, I added the checking component; checked for four horizontals or four verticals in a row for a team

--- member2_development.py
+++ member2_development.py
@@ -1,1 +1,141 @@
 
+import arcade
+import time
+
+WIDTH = 50
+HEIGHT = 50
+
+state = 0
+grid = []
+temp = []
+n = 1
+for i in range(8):
+   if len(temp) > 1:
+       grid.append(temp)
+   temp = []
+   for j in range(6):
+       temp.append(0)
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Connect Four"
+team = True
+curr = True
+x_chips = [0] * len(grid)
+y_chips = [0] * len(grid[1])
+
+
+
+class MyGame(arcade.Window):
+      """ Main application class. """
+
+      def __init__(self):
+          super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+          self.shape_list = None
+          self.radius = 20
+
+      def closest(self, x):
+          d = x
+          l = x_chips
+          n = ([abs(i - d) for i in l])
+          return (n.index(min(n)))
+
+      def get_y(self, x_clicked):
+          l = grid[x_clicked]
+          n = 0
+          for i in l:
+              if i == 0:
+                  return (n)
+                  break
+              n += 1
+
+      def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+          global curr
+          global state
+          if state == 0:
+              x_clicked = (self.closest(x))
+              y_empty = self.get_y(x_clicked)
+              if curr == True:
+                  grid[x_clicked][y_empty] = 2
+                  curr = False
+              else:
+                  grid[x_clicked][y_empty] = 1
+                  curr = True
+
+      def check_win(self):
+          global team
+          global state
+          boardHeight = len(grid[0])
+          boardWidth = len(grid)
+          if curr:
+              tile = 1
+          else:
+              tile = 2
+
+          # check horizontal spaces
+          for y in range(boardHeight):
+              for x in range(boardWidth - 3):
+                  if tile != 0 and grid[x][y] == tile and grid[x + 1][y] == tile and grid[x + 2][y] == tile and \
+                          grid[x + 3][y] == tile:
+                      team = (curr)
+                      print("won")
+                      state = 1
+                      return True
+
+          # check vertical spaces
+          for x in range(boardWidth):
+              for y in range(boardHeight - 3):
+                  if tile != 0 and grid[x][y] == tile and grid[x][y + 1] == tile and grid[x][y + 2] == tile and \
+                          grid[x][y + 3] == tile:
+                      team = (curr)
+                      print("won")
+                      state = 1
+                      return True
+
+
+      def on_draw(self):
+          """
+          Render the screen.
+          """
+          arcade.start_render()
+          margin_x = 155
+          margin_y = 180
+          for i in range(len(grid)):
+              for j in range(len(grid[i])):
+                  x_chips[i] = (margin_x + i * WIDTH)
+                  y_chips[j] = (margin_y + j * HEIGHT)
+                  self.check_win()
+                  if state == 0:
+                      if curr == True:
+                          turn = "Blue's Turn Now"
+                      else:
+                          turn = "Red's Turn Now"
+                      arcade.draw_text(turn, 215, 135, arcade.color.WHITE, 16)
+                  if state == 1:
+                      if curr == True:
+                          turn = "Red Won"
+                          arcade.draw_text(turn, 260, 480, arcade.color.RED, 16)
+
+                      else:
+                          turn = "Blue Won"
+                          arcade.draw_text(turn, 260, 480, arcade.color.BLUE, 16)
+
+
+                  arcade.draw_rectangle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH, WIDTH,
+                                               arcade.color.WHITE)
+                  if grid[i][j] == 0:
+                      arcade.draw_circle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH // 2,
+                                                arcade.color.BLACK)
+                  if grid[i][j] == 1:
+                      arcade.draw_circle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH // 2,
+                                                arcade.color.RED)
+                  if grid[i][j] == 2:
+                      arcade.draw_circle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH // 2,
+                                               arcade.color.BLUE)
+
+def main():
+    window = MyGame()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
