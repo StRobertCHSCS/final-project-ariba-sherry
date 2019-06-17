e88a4dd7d6c94cab3d225fbfac0cc85d67e1a4a9
sherrycao0 <48096482+sherrycao0@users.noreply.github.com>
2019-06-17 19:06:30 -0400

    Connect4: Finished the animation for the chips "sliding" down the grid to an empty slot

--- member1_development.py
+++ member1_development.py
@@ -1,98 +1,117 @@
 import arcade
 import time
 
+
 WIDTH = 50
-HEIGHT = 50
 
-state = 0
-grid = []
-temp = []
-n = 1
-for i in range(8):
-   if len(temp) > 1:
-       grid.append(temp)
-   temp = []
-   for j in range(6):
-       temp.append(0)
 
+
+grid = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Connect Four"
-team = True
+
+
 curr = True
-x_chips = [0] * len(grid)
-y_chips = [0] * len(grid[1])
+x_chips = [0]*len(grid)
+
+def switchTeam(x):
+    global curr
+    if curr:
+        grid[0][x] = 1
+        curr = False
+    else:
+        grid[0][x] = 2
+        curr = True
+
+
 
 
 
 class MyGame(arcade.Window):
-      """ Main application class. """
+    """ Main application class. """
 
-      def __init__(self):
-          super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-          self.shape_list = None
-          self.radius = 20
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        self.shape_list = None
+        self.radius = 20
 
-      def closest(self, x):
-          d = x
-          l = x_chips
-          n = ([abs(i - d) for i in l])
-          return (n.index(min(n)))
 
-      def get_y(self, x_clicked):
-          l = grid[x_clicked]
-          n = 0
-          for i in l:
-              if i == 0:
-                  return (n)
-                  break
-              n += 1
+    def update(self, dt):
+        """ Move everything """
 
-      def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-          global curr
-          global state
-          if state == 0:
-              x_clicked = (self.closest(x))
-              y_empty = self.get_y(x_clicked)
-              if curr == True:
-                  grid[x_clicked][y_empty] = 2
-                  curr = False
-              else:
-                  grid[x_clicked][y_empty] = 1
-                  curr = True
+        # if no team has won
+
+        if not win():
+            if not self.draw():
+                for i in range(1, len(grid)):
+                    for j in range(len(grid[i])):
+                        # check if after is empty slot
+                        if grid[i-1][j] != 0 and grid[i][j] == 0:
+                            # chip falls down
+                            grid[i][j] = grid[i-1][j]
+                            grid[i - 1][j] = 0
+        else:
+            time.sleep(1)
+            for i in range(len(grid)):
+                for j in range(len(grid[i])):
+                    grid[i][j] = 0
 
 
 
 
-      def on_draw(self):
-          """
-          Render the screen.
-          """
-          arcade.start_render()
-          margin_x = 155
-          margin_y = 180
-          for i in range(len(grid)):
-              for j in range(len(grid[i])):
-                  x_chips[i] = (margin_x + i * WIDTH)
-                  y_chips[j] = (margin_y + j * HEIGHT)
+    def on_key_press(self, symbol: int, modifiers: int):
+        keys = [49 + i for i in range(len(grid[0]))]
+        for i in range(len(keys)):
+            if symbol == keys[i] and grid[0][i] == 0:
+                switchTeam(i)
 
 
 
-                  arcade.draw_rectangle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH, WIDTH,
-                                               arcade.color.WHITE)
-                  if grid[i][j] == 0:
-                      arcade.draw_circle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH // 2,
-                                                arcade.color.BLACK)
-                  if grid[i][j] == 1:
-                      arcade.draw_circle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH // 2,
-                                                arcade.color.RED)
-                  if grid[i][j] == 2:
-                      arcade.draw_circle_filled(margin_x + i * WIDTH, margin_y + j * WIDTH, WIDTH // 2,
-                                               arcade.color.BLUE)
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        if not win():
+            arcade.start_render()
+            margin_x = SCREEN_WIDTH//2 - WIDTH*2
+            margin_y = SCREEN_HEIGHT//2 - WIDTH*2
+
+            for i in range(len(grid)):
+                for j in range(len(grid[i])):
+
+                    x_chips[j] = (margin_x + j*WIDTH)
+
+                    arcade.draw_rectangle_filled(margin_x + i*WIDTH, margin_y*2 + -j *WIDTH, WIDTH, WIDTH, arcade.color.WHITE)
+
+                    if grid[j][i] == 0:
+                        arcade.draw_circle_filled(margin_x + i*WIDTH, margin_y*2 + -j*WIDTH, WIDTH//2, arcade.color.BLACK)
+
+                    if grid[j][i] == 1:
+                        arcade.draw_circle_filled(margin_x + i*WIDTH, margin_y*2 +-j*WIDTH, WIDTH//2, arcade.color.RED)
+
+                    if grid[j][i] == 2:
+                        arcade.draw_circle_filled(margin_x + i*WIDTH, margin_y*2 + -j*WIDTH, WIDTH//2, arcade.color.BLUE)
+
+
+
+    def draw(self):
+        return False not in [j != 0 for i in grid for j in i]
+
 
 def main():
     window = MyGame()
     arcade.run()
 
+
 if __name__ == "__main__":
+
     main()
+    print(x_chips)
